8e805a0883a4fa332aa088af9420860f4ae82ff8
chrisk <ckv9508@saintjoe.edu>
2016-02-17 14:30:02 -0500

    Commit and Push

---
+++ ExamPracticeCMK/src/edu/saintjoe/cs/chrisk/ExamPracticeCMK/ExamPracticeCMK.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.chrisk.ExamPracticeCMK;
+
+public class ExamPracticeCMK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Commit and Push Comment
+	}
+
+}
